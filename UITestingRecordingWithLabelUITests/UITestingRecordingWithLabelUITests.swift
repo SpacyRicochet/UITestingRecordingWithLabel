283f1d28888b0d74aa24c94eb5c5534779c00f80
Bruno Scheele <bruno@noodlewerk.com>
2015-06-10 15:50:58 -0700

    Submitted rdar://21328058

--- UITestingRecordingWithLabelUITests/UITestingRecordingWithLabelUITests.swift
+++ UITestingRecordingWithLabelUITests/UITestingRecordingWithLabelUITests.swift
@@ -30,6 +30,9 @@ class UITestingRecordingWithLabelUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+
+        
     }
     
 }
